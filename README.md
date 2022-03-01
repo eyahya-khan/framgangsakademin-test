cc78b939ca3524d745a7dc5e4a13b3a10830d88b
eyahya-khan <eyahyakhan@gmail.com>
2022-03-01 23:21:19 +0100

    update all

---
+++ README.md
@@ -0,0 +1,8 @@
+# Främgangsakademin Test
+
+1. HTML5, CSS3 and JavaScript
+2. API call using fetch()
+
+# view on live
+
+https://trusting-meitner-6b21ee.netlify.app
--- js/script.js
+++ js/script.js
@@ -14,20 +14,20 @@ fetch(url)
     const vodeoUrl  = data.data.heroBanner.videoUrl
     const profiles  = data.data.profiles
 
-displayHeaderInfo.innerHTML =`
-<div class="headerTitle-info">
-<h1>${title}</h1>
-<p>${titleInfo}</p>
-</div>
-<div class="headerVideo">
-<video width="200" controls>
-  <source src=${vodeoUrl} type="video/mp4">
-</video>
-</div>
-`
+  displayHeaderInfo.innerHTML =`
+                <div class="headerTitle-info">
+                  <h1>${title}</h1>
+                  <p>${titleInfo}</p>
+                </div>
+                <div class="headerVideo">
+                  <video width="200" controls autoplay muted>
+                    <source src=${vodeoUrl} type="video/mp4">
+                  </video>
+                </div>
+                `
 
-profiles.forEach(item => {
-    //console.log(item.firstName)
+  profiles.forEach(item => {
+    //console.log(item)
 
     const firstName = item.firstName
     const lastName  = item.lastName
@@ -35,14 +35,14 @@ profiles.forEach(item => {
     const imageUrl  = item.imgUrl
 
     displaySectionInfo.innerHTML += `
-    <div>
-    <div><img src=${imageUrl} alt=${firstName} ${lastName} width="300" height="300"/></div>
-    <div>
-    <h2>${firstName} ${lastName}</h2>
-    <p>${expertise}</p>
-    </div>
-    </div>
-    `
+                  <div class="sectionInfo">
+                        <img src=${imageUrl} alt=${firstName}${lastName} width="300" height="300"/>
+                    <div class="sectionInfo-name">
+                        <h2>${firstName} ${lastName}</h2>
+                        <p>${expertise}</p>
+                    </div>
+                  </div>
+                  `
 });
 
 })
